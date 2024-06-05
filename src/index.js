bf75ccc28ba2b2c07961ccaac4f23c6f88a210ed
Sameer <nischalkarn369369@gmail.com>
2024-06-05 05:05:09 +0300

    [+] Google OAuth Configured

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* USE .env this is only for testing */}
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_OAUTH_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
