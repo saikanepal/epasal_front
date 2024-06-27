189a90eff3a706deaaaaaeaf296266d5e4f15779
rose-rawal <rooserwl@gmail.com>
2024-06-27 22:40:22 +0545

    scroll effect created

--- src/HomePage/Header/Heading.js
+++ src/HomePage/Header/Heading.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import ghost from '../../Assets/Ghost.png'
-import { IoBagHandleOutline } from "react-icons/io5";
+
 
 const Heading = ({ setnavbarImage }) => {
   const headingRef = useRef(null);
@@ -38,18 +37,11 @@ const Heading = ({ setnavbarImage }) => {
       />
       <div className=" fixed left-0 w-[98%] h-full bg-black opacity-70 -z-10 top-2 left-1/2 transform -translate-x-1/2 rounded-xl"></div>
       <div className="relative z-0 h-[450px] text-white flex flex-col gap-5 items-center justify-center uppercase">
-        <h3 className='text-6xl font-semibold tracking-wide '>Express Your <span className='font-Zen-Tokyo-Zoo font-normal'>Creativity</span></h3>
-        <p>Build Your Own Ecommerce Website</p>
+        <h3 className='text-3xl md:text-6xl font-semibold tracking-wide text-center'>Express Your <span className='font-Zen-Tokyo-Zoo font-normal'>Creativity</span></h3>
+        <p className='text-sm md:text-base text-center'>Build Your Own Ecommerce Website</p>
       </div>
 
-      <div className='relative flex justify-center -mb-16 z-10'>
-        <img src={ghost} className='-mt-[6px] absolute -z-10 h-40' />
-        <div className=' w-[130px] h-[130px] bg-[#8E410A] rounded-full border border-[15px] border-[#FEFBF6] flex flex-col items-center justify-center text-center font-bold text-white'>
-
-          <IoBagHandleOutline className='text-3xl mb-2' />
-          <p className='text-sm font-light'>Build Your Store</p>
-        </div>
-      </div>
+      
     </div>
   )
 }
