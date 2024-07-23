23ffa429656ce292d88ff94ce0105a976a1ee6b0
ShopAtBanau <shopatbanau@gmail.com>
2024-07-23 10:51:08 +0545

    fixed the music player button

--- src/Theme/Theme1/AudioPlayer/ModernReactPlayer.js
+++ src/Theme/Theme1/AudioPlayer/ModernReactPlayer.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
-import { FaPlay, FaPause } from 'react-icons/fa';
+import { FaPlay, FaPause, FaMusic } from 'react-icons/fa';
 import { useStore } from '../T1Context';
+
 const ModernReactPlayer = () => {
     const [volume, setVolume] = useState(0.5);
     const [playing, setPlaying] = useState(false);
     const { store, setStore } = useStore();
-
 
     const handleVolumeChange = (e) => {
         setVolume(parseFloat(e.target.value));
@@ -28,10 +28,11 @@ const ModernReactPlayer = () => {
                     volume={volume}
                     loop={true}
                 />
-                <div className="fixed bottom-4 left-4 flex items-center p-2 sm:p-3 bg-gray-900 bg-opacity-50 rounded-full shadow-lg backdrop-blur-lg transition-colors duration-300 hover:bg-opacity-75">
+                <div className="fixed bottom-4 left-4 flex items-center p-1 sm:p-2 bg-gray-900 bg-opacity-10 rounded-full shadow-lg backdrop-blur-lg transition-all duration-300 hover:bg-opacity-75 hover:scale-105 group">
+                    <FaMusic className="text-white text-lg sm:text-xl opacity-30 group-hover:opacity-0 transition-opacity duration-300" />
                     <button
                         onClick={togglePlayPause}
-                        className="text-white text-base sm:text-lg bg-blue-600 p-2 rounded-full hover:bg-blue-700 focus:outline-none shadow-md transition-transform transform hover:scale-105"
+                        className="text-white text-sm sm:text-base bg-blue-600 p-1 sm:p-2 rounded-full focus:outline-none shadow-md transition-transform transform hover:scale-110 opacity-0 group-hover:opacity-100"
                     >
                         {playing ? <FaPause /> : <FaPlay />}
                     </button>
@@ -42,7 +43,7 @@ const ModernReactPlayer = () => {
                         step="0.01"
                         value={volume}
                         onChange={handleVolumeChange}
-                        className="ml-2 sm:ml-3 w-20 sm:w-24 h-1 bg-gray-300 rounded-lg cursor-pointer focus:outline-none appearance-none"
+                        className="ml-1 sm:ml-2 w-16 sm:w-20 h-1 bg-gray-300 rounded-lg cursor-pointer focus:outline-none appearance-none opacity-0 group-hover:opacity-100"
                         style={{
                             backgroundImage: 'linear-gradient(to right, #3b82f6, #3b82f6)',
                             backgroundSize: `${volume * 100}% 100%`,
@@ -52,11 +53,8 @@ const ModernReactPlayer = () => {
                 </div>
             </div>
         );
-    }
-    else{
-        return(
-            null
-        )
+    } else {
+        return null;
     }
 };
 
