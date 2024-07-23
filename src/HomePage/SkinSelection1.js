9bb55a6c42d1efb8218469e78073f4c4cc322150
ByapakSigdel <sigdelmb123@gmail.com>
2024-07-23 21:35:47 +0545

    Fixed the skin section in homepage

--- src/HomePage/SkinSelection1.js
+++ src/HomePage/SkinSelection1.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for react-router-dom v6
 import SingleImage from '../Assets/SingleImage1.webp'; // Import your image
+import SingleImageMd from '../Assets/FrameMd.webp'; // Import your image
 
 const SkinSection1 = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/buildstore');
+  };
+
   return (
     <div className="flex justify-center items-center bg-white p-8 py-8 md:py-10 lg:py-20">
-    <img src={SingleImage} alt="Single Image" className="w-full h-auto max-w-[1200px] object-contain" />
+      <img
+        src={SingleImage}
+        alt="Single Image"
+        className="hidden lg:block w-full h-auto max-h-[800px] object-contain cursor-pointer"
+        onClick={handleClick}
+      />
+      <img
+        src={SingleImageMd}
+        alt="Single Image Medium"
+        className="block lg:hidden w-full h-auto max-h-[800px] object-contain cursor-pointer"
+        onClick={handleClick}
+      />
     </div>
   );
 };
