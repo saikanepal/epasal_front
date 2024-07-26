2bf81672e32429b3ac7af0f2ce69a8d8cb8b1180
ShopAtBanau <shopatbanau@gmail.com>
2024-07-26 20:55:28 +0545

    random navigation fix

--- src/Components/SecondaryBanner/SecondaryBanner1.js
+++ src/Components/SecondaryBanner/SecondaryBanner1.js
@@ -5,7 +5,7 @@ import { useDropzone } from "react-dropzone";
 import { useStore } from "../../Theme/Theme1/T1Context"; // Adjust the path as necessary
 import { useImage } from "../../Hooks/useImage";
 import { Link } from "react-router-dom";
-const SecondaryBanner1 = ({ previewMode,isEdit, defaultBgImage,storeName }) => {
+const SecondaryBanner1 = ({ previewMode, isEdit, fetchedFromBackend, defaultBgImage, storeName }) => {
   const { store, setStore } = useStore();
   const { color, secondaryBannerText } = store;
   const { uploadImage } = useImage();
@@ -27,14 +27,9 @@ const SecondaryBanner1 = ({ previewMode,isEdit, defaultBgImage,storeName }) => {
     };
     reader.readAsDataURL(backgroundImage);
   };
-const handleExploreClick=(e)=>{
 
-  if (store.fetchedFromBackend && !store.isEdit){
-    navigate(`${process.env.REACT_APP_BASE_URL}/store/products/:${storeName}`)
-    
-  }
-  
-}
+
+
   const handleTextChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
@@ -54,19 +49,20 @@ const handleExploreClick=(e)=>{
   } = useDropzone({ onDrop: onDropBackground });
 
   useEffect(() => {
-    
+
   }, [store])
   return (
     <div className="box-border py-8 relative mb-16" style={{
-      backgroundColor: color.secondaryBannerColor.backgroundThemeColor1,}}>
+      backgroundColor: color.secondaryBannerColor.backgroundThemeColor1,
+    }}>
       <motion.div
         className="box-border font-roboto  shadow-lg min-h-[300px] sm:min-h-[350px] md:min-h-[400px] lg:min-h-[400px] flex flex-col sm:flex-col md:flex-row lg:flex-row justify-between items-center text-black"
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
         style={{
-          
-          fontFamily:store?.fonts?.Banner2,
+
+          fontFamily: store?.fonts?.Banner2,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -107,7 +103,7 @@ const handleExploreClick=(e)=>{
                 onChange={handleTextChange}
                 className="text-3xl lg:text-5xl xl:text-6xl font-bold mb-2 w-[90%] border border-gray-500"
                 style={{ color: color.secondaryBannerColor.textColor }}
-                
+
               />
               <textarea
                 name="paragraph"
@@ -121,24 +117,21 @@ const handleExploreClick=(e)=>{
           )}
 
 
-            <button
-
+          <button
             className={`mt-4 md:mt-2 py-auto lg:mt-8  h-12 rounded-lg border-2 border-success px-6 py-auto text-xs md:text-xs lg:text-base font-medium uppercase leading-normal text-success transition duration-150 ease-in-out hover:border-success-600 hover:bg-success-50/50 hover:text-success-600 focus:border-success-600 focus:bg-success-50/50 focus:text-success-600 focus:outline-none focus:ring-0 active:border-success-700 active:text-success-700 motion-reduce:transition-none hover:border-2 flex items-center
-               ${
-              !previewMode ? "cursor-not-allowed opacity-50" : ""
-            }`}
+               ${!previewMode ? "cursor-not-allowed opacity-50" : ""
+              }`}
             style={{
               color: color.secondaryBannerColor.buttonText,
               backgroundColor: color.secondaryBannerColor.buttonColor,
             }}
-          
-          
           >
-            <Link to={`/store/products/${store.name}`}>
-            Explore More
+
+            <Link to={!isEdit && fetchedFromBackend && `/store/products/${store.name}`}>
+              Explore More
             </Link><span className="text-lg ml-3 my-auto">&gt;</span>
-            </button>
-         
+          </button>
+
         </div>
         {(!previewMode) && (
           <div className="absolute top-0 left-0 mt-10 mr-2">
--- src/Theme/Theme1/T1OfferBanner.js
+++ src/Theme/Theme1/T1OfferBanner.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { useStore } from "./T1Context";
 import OfferBanner1 from "../../Components/OfferBanner/OfferBanner1";
@@ -5,10 +6,10 @@ import offerBanner from "../../Assets/offerbanner.webp";
 
 const T1OfferBanner = () => {
   const { store } = useStore();
-  const { previewMode,isEdit } = store;
+  const { previewMode,isEdit ,fetchedFromBackend } = store;
 
   return (
-    <OfferBanner1 previewMode={previewMode} isEdit={isEdit} defaultBgImage={offerBanner} />
+    <OfferBanner1 previewMode={previewMode} isEdit={isEdit} fetchedFromBackend={fetchedFromBackend} defaultBgImage={offerBanner} />
   );
 };
 
