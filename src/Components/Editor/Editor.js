487762a656cc0e0e238013ee461ba3fe83ed2d23
Sahil Karn <sahilkarn03@gmail.com>
2024-08-18 23:34:11 -0400

    product removal tip

--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -1635,6 +1635,9 @@ const Editor = ({ handleDesignClick, handleContentClick, currentStep, instructio
                 </li>
                 <li className='font-semibold border-b-2 border-gray-200 pb-5 font-Poppins hover:bg-gray-50 p-4 rounded-lg shadow-md transition duration-300'>
                   Add Products<br />
+                  <Tooltip message="Remove the product by clicking the red cross on the product itself">
+                      <span className='  absolute left-[104px] bottom-[0px] ml-2  flex   text-yellow-600 text-2xl font-bold'>?</span>
+                    </Tooltip>
                   <div className='mt-2'>
                     <button className='px-2 text-[12px]  font-Ubuntu border-2 border-gray-300' onClick={handleAddProduct}>Add +</button>
                   </div>
