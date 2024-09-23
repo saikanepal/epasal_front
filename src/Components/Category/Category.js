e92b3005ee5c5bc0b7b68f49866ca4310748b2fa
rose-rawal <rooserwl@gmail.com>
2024-09-23 20:00:00 +0545

    auto theme

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -3,29 +3,30 @@ import Category1 from './Category1';
 import Theme2Category1 from './Theme2/Category1'
 
 const Category = ({ categoryType,activeTheme, ...props }) => {
-    // switch(activeTheme){
-    //     case 1:
-    //         switch (categoryType) {
-    //             case 'Category1':
-    //                 return <Category1 {...props} />;
-    //             default:
-    //                 return <div>Unknown category type</div>;
-    //         }
-    //     case 2:
-    //         switch (categoryType) {
-    //            case 'Category1':
+    console.log("active theme",activeTheme)
+    switch(activeTheme){
+        case 1:
+            switch (categoryType) {
+                case 'Category1':
+                    return <Category1 {...props} />;
+                default:
+                    return <div>Unknown category type</div>;
+            }
+        case 2:
+            switch (categoryType) {
+               case 'Category1':
                     return <Theme2Category1 {...props} />;
-    //             default:
-    //                 return <div>Unknown category type</div>;
-    //         }
-    //     default:
-    //         switch (categoryType) {
-    //             case 'Category1':
-    //                 return <Category1 {...props} />;
-    //             default:
-    //                 return <div>Unknown category type</div>;
-    //         }
-    // }
+                default:
+                    return <div>Unknown category type</div>;
+            }
+        default:
+            switch (categoryType) {
+                case 'Category1':
+                    return <Category1 {...props} />;
+                default:
+                    return <div>Unknown category type</div>;
+            }
+    }
 };
 
 export default Category;
--- src/Components/SubProductCard/SubProductCard.js
+++ src/Components/SubProductCard/SubProductCard.js
@@ -7,41 +7,41 @@ import Theme2SubProduct1 from './Theme2/SubProductCard1'
 const SubProductCard = ({ categoryType, activeTheme,...props }) => {
     
     
-    //switch (activeTheme){
-        // case 1:
-        //     switch (categoryType) {
-        //         case 'default':
-        //             return <SubProductCard1 {...props} />;
-        //         case 'Modern Minimalistic':
-        //             return <SubProductCard3 {...props} />
-        //         case 'Slider':
-        //             return <SubProductCard2 {...props} />
-        //         case 'Sleek':
-        //             return <SubProductCard4 {...props} />
-        //         default:
-        //             return <SubProductCard1 {...props} />;
-        //     }
-        // case 2:
-        //     switch (categoryType) {
-        //         case 'default':
+    switch (activeTheme){
+        case 1:
+            switch (categoryType) {
+                case 'default':
+                    return <SubProductCard1 {...props} />;
+                case 'Modern Minimalistic':
+                    return <SubProductCard3 {...props} />
+                case 'Slider':
+                    return <SubProductCard2 {...props} />
+                case 'Sleek':
+                    return <SubProductCard4 {...props} />
+                default:
+                    return <SubProductCard1 {...props} />;
+            }
+        case 2:
+            switch (categoryType) {
+                case 'default':
                     return <Theme2SubProduct1 {...props} />;
-    //             default:
-    //                 return <Theme2SubProduct1 {...props} />;
-    //         }
-    //     default:
-    //         switch (categoryType) {
-    //             case 'default':
-    //                 return <SubProductCard1 {...props} />;
-    //             case 'Modern Minimalistic':
-    //                 return <SubProductCard3 {...props} />
-    //             case 'Slider':
-    //                 return <SubProductCard2 {...props} />
-    //             case 'Sleek':
-    //                 return <SubProductCard4 {...props} />
-    //             default:
-    //                 return <SubProductCard1 {...props} />;
-    //         }
-    // }
+                default:
+                    return <Theme2SubProduct1 {...props} />;
+            }
+        default:
+            switch (categoryType) {
+                case 'default':
+                    return <SubProductCard1 {...props} />;
+                case 'Modern Minimalistic':
+                    return <SubProductCard3 {...props} />
+                case 'Slider':
+                    return <SubProductCard2 {...props} />
+                case 'Sleek':
+                    return <SubProductCard4 {...props} />
+                default:
+                    return <SubProductCard1 {...props} />;
+            }
+    }
 }
 
 export default SubProductCard
